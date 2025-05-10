75aeb55709b6bb5af85fd1d3398bf5db8dcbb37e
Celeevo <korn2052@gmail.com>
2025-05-10 20:01:35 +0300

    10-05-25

--- 03 moex_bt_test.py
+++ 03 moex_bt_test.py
@@ -8,13 +8,16 @@ from moex_store import MoexStore
 # import datetime
 
 def runstrat():
-    contracts = ['EuH3', 'EuM3', 'EuU3', 'EuZ3', 'EuH4', 'EuM4', 'EuU4']
+    contracts = ['EuM5',]
+    contracts = ['SBER',]
     datas = []
     for sec_id in contracts:
         tf = '15m'
         store = MoexStore()
-        fromdate = store.futures.prevexpdate(sec_id)
-        todate = store.futures.expdate(sec_id)
+        # fromdate = store.futures.prevexpdate(sec_id)
+        fromdate = datetime.today() - timedelta(days=30)
+        todate = datetime.today()
+        # todate = store.futures.expdate(sec_id)
         # data = store.getdata(sec_id=sec_id,
         datas.append(store.getdata(sec_id=sec_id,
                              fromdate=fromdate,
